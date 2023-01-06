fd0e49e81b03f7b9aa80b5a7bdb6d81005a2d808
Keisuke Saito <75734741+Yoshina0624@users.noreply.github.com>
2023-01-06 15:08:37 +0900

    Add files via upload

---
+++ river-portfolio-main/js/slider.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.m_slider').slick({
+        dots: true,
+        centerMode: true,
+        centerMargin: '30px',
+        arrows: false,
+        variableWidth: true
+    });
+});
