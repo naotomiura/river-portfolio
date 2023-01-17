64515b598b7f3f24700d76c76153ede07df7abfb
naotomiura <85100588+naotomiura@users.noreply.github.com>
2023-01-17 17:39:10 +0900

    Delete river-portfolio-main directory

--- river-portfolio-main/js/slider.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('.m_slider').slick({
-        dots: true,
-        centerMode: true,
-        centerMargin: '30px',
-        arrows: false,
-        variableWidth: true
-    });
-});
